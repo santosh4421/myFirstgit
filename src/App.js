66fab73716568e84bfcbcd8df58f3ed9fad7ebac
santosh4421 <santoshdaberao1610@mail.com>
2023-04-12 09:59:44 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
 
-function App() {
+
+export default function App() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [number, setNumber] = useState('');
+  const [values,setValues]=useState([])
+  
+  console.log(email)
+  console.log(name)
+
+  function handleClick(){
+    setValues([...setValues,name,email,number])
+    // setValues([...setName,name])
+  }
+console.log(setValues)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="main">
+        <div className="main1">
+          
+          <div className='main11'>
+        
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} /><br/>
+      
+       
+        
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} /><br/>
+    
+      
+        
+      <input type="tel" value={number} onChange={(e) => setNumber(e.target.value)} /><br/>
+     
+        <button type="submit" onClick={handleClick}>Submit</button>
+        </div>
+      
+          
+          <div className='main12'>
+          {name}<br/>
+             {email}<br/>
+             {number}
+          </div>
+          </div>
+        
+        
+        <div className="main2">
+        <ul>
+            {values.map((ele)=>{
+              return(<li>{ele}</li>)
+            })}
+            </ul>
+        </div>
+        </div>
+  </div>
+      
+      
+    
   );
 }
-
-export default App;
