fe40fe130cd220119c29c11c0fc092e4d586b20f
santosh4421 <santoshdaberao1610@mail.com>
2023-05-03 11:47:43 +0530

    serachabledropdown

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ import { Media } from './Media';
 
 function App() {
   return (
-   
-    <Media/>
+   <>
+    {/* <Media/> */}
+    <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+    <form>
+      <input list="option"/>
+        <datalist id="option">
+            <option>option1</option>
+            <option>option2</option>
+            <option>option3</option>
+            <option>option4</option>
+        </datalist>
+    </form>
+    </div>
+    </>
   );
 }
 
